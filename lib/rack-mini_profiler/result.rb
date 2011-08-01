256bea40f02c5f1fe546d05cb7f250caa8d75228
Kevin Pang <kpanghmc@gmail.com>
2011-08-01 02:44:14 -0700

    Made result.id readonly since it should only be set on initialize

--- lib/rack-mini_profiler/result.rb
+++ lib/rack-mini_profiler/result.rb
@@ -3,7 +3,8 @@ require "uuidtools"
 module Rack
   class MiniProfiler
     class Result
-      attr_accessor :id, :response_time
+      attr_accessor :response_time
+      attr_reader :id
       
       def initialize
         @id = UUIDTools::UUID.random_create.to_s
